b19b72b8b6c043806c2a3b1615c89451eed50ab7
Roman <roomen817@gmail.com>
2024-02-24 22:56:48 +0100

    c

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const TEST_API_BASE_URL = 'http://localhost:8080';
 const PROD_API_BASE_URL = 'https://veteran-api-production.up.railway.app';
 
 const api = axios.create({
-  baseURL: TEST_API_BASE_URL,
+  baseURL: PROD_API_BASE_URL,
 });
 
 export default api;
