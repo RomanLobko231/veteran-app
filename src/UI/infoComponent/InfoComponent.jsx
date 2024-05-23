032968abc5f1f6e79bad164445def579de8bcb87
Roman <roomen817@gmail.com>
2024-05-23 21:28:38 +0200

    cosmetic changes

--- src/UI/infoComponent/InfoComponent.jsx
+++ src/UI/infoComponent/InfoComponent.jsx
@@ -13,7 +13,7 @@ const InfoComponent = ({title, date, description}) => {
 
   return (
     <div className={cl.container}>
-    <h1>{title}</h1>
+    <h2>{title}</h2>
     <p className={cl.date}>{formattedCreatedDate}</p>
     <hr style={{width: '55%', maxWidth: "2300px"}}/>
     <p style={{ whiteSpace: "pre-line" }} className={cl.description}>{description}</p>
