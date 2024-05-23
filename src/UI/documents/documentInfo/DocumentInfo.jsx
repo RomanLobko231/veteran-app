72f5a280b9d6509cf794210bd0e06994752bff8a
Roman <roomen817@gmail.com>
2024-05-23 22:31:56 +0200

    + document display

--- src/UI/documents/documentInfo/DocumentInfo.jsx
+++ src/UI/documents/documentInfo/DocumentInfo.jsx
@@ -1,10 +1,14 @@
 
+import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import InfoComponent from '../../infoComponent/InfoComponent';
 import cl from './DocumentInfo.module.css'
 import { TbFileDownload } from "react-icons/tb";
+import { useEffect, useState } from 'react';
 
 
 const DocumentInfo = ({ downloadedFile }) => {
+
+    const [displayFile, setDisplayFile] = useState(null);
 
     const downloadFile = () => {
         const link = document.createElement('a');
@@ -15,12 +19,38 @@ const DocumentInfo = ({ downloadedFile }) => {
         document.body.removeChild(link);
     }
 
+    const base64ToBlob = (base64, mimeType) => {
+        const byteCharacters = atob(base64);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        return new Blob([byteArray], { type: mimeType });
+    }
+
+    useEffect(() => {
+        const blob = base64ToBlob(downloadedFile.file, downloadedFile.mime_type);
+        setDisplayFile(blob);
+    }, []);
+
     return (
         <div className={cl.page}>
-            <InfoComponent title={downloadedFile.title} date={downloadedFile.date} description={downloadedFile.description}/>
+            <InfoComponent title={downloadedFile.title} date={downloadedFile.date} description={downloadedFile.description} />
             <div className={cl.button} onClick={downloadFile}>
                 <TbFileDownload className={cl.icon} />
                 <p>Завантажити файл</p>
+            </div>
+            <div>
+                <DocViewer
+                    document={
+                        {
+                            uri: window.URL.createObjectURL(file),
+                            fileName: file.name
+                        }
+                    }
+                    pluginRenderers={DocViewerRenderers}
+                />
             </div>
         </div>
     );
